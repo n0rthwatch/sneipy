793c2f4b0996608ce9eeb4032d38070debb47a4b
n0rthwatch <polka1416@gmail.com>
2024-08-23 21:13:17 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+const tg = window.Telegram.WebApp;
+
+
+</script>
+
+<template>
+  <RouterView />
+  <button @click="tg.close()">Закрыть</button>
+</template>
+
+<style scoped>
+
+</style>
